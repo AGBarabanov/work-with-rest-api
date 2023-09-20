1f32a7c73a27ded9299cc9df00a5db93c437f3b2
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-09-20 14:03:40 +0300

    added api tests

--- src/test/java/com/barabanov/tests/ApiTests.java
+++ src/test/java/com/barabanov/tests/ApiTests.java
@@ -108,4 +108,49 @@ public class ApiTests {
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
     }
+
+
+    @Test
+    public void addToCartTest(){
+        String cookieValue = "41DB9E11694DABC6EBF2714400B4978FCA4CC0242ADDC96E2080FB265D2FFE913F5A7065564906A2B4C8D1B19065F24CD13C8566E888B29DC2E582DF05C2272AD2C3470AB40C5A943F12AA4E7581FB7C31CBC767CAF086FCAAF8EA0D374A2C886459CBA943F4D947CB1767A3B31CAD088FD77A6FEEB8AFBFE85921EDEAB98BCD64F8F9351AB0B1B89DE6BF714DDC37BA;",
+                body = "product_attribute_75_5_31=96" +
+                        "&product_attribute_75_6_32=100" +
+                        "&product_attribute_75_3_33=102" +
+                        "&product_attribute_75_8_35=108" +
+                        "&addtocart_75.EnteredQuantity=2";
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("NOPCOMMERCE.AUTH", cookieValue)
+                .body(body)
+                .when()
+                .post("https://demowebshop.tricentis.com/addproducttocart/details/75/1")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("message", hasToString("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .body("success", is(true) );
+    }
+
+    @Test
+    public void addToCartAnonymTest(){
+
+                String body = "product_attribute_75_5_31=96" +
+                        "&product_attribute_75_6_32=100" +
+                        "&product_attribute_75_3_33=102" +
+                        "&product_attribute_75_8_35=108" +
+                        "&addtocart_75.EnteredQuantity=2";
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body(body)
+                .when()
+                .post("https://demowebshop.tricentis.com/addproducttocart/details/75/1")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("message", hasToString("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .body("success", is(true) )
+                .body("updatetopcartsectionhtml", is("(2)"));
+    }
 }
